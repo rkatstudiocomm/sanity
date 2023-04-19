d9dd97c1edd15291a0c1df2ebdeb64fa22c297f5
rkyerevan <rkyerevan@gmail.com>
2023-04-18 20:15:31 -0700

    app files added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({
+                                       children
+                                   }: {
+    children: React.ReactNode
+}) {
     return (
         <html lang="en">
         <body>
         {children}
         </body>
         </html>
-    )};
+    )
+};
